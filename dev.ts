121e56bc505a1711d1d043a9e653982a44765156
Frank Chen <frank.chen@iko.com>
2026-06-06 16:07:27 -0400

    Fix dev.ts to use gen() function

--- dev.ts
+++ dev.ts
@@ -1,12 +1,6 @@
 #!/usr/bin/env -S deno run -A --unstable-kv
 
-import "$fresh/server.ts";
+import { gen } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-import { fresh } from "$fresh/server.ts";
-
-await fresh(manifest, {
-  onBeforeStart: () => {
-    console.log("Fresh app built successfully");
-  },
-});
+await gen(manifest);
