e8575276b310f73bd64714c96ad371b15879dfe1
Frank Chen <frank.chen@iko.com>
2026-06-06 16:08:44 -0400

    Fix dev.ts import path for gen function

--- dev.ts
+++ dev.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run -A --unstable-kv
 
-import { gen } from "$fresh/server.ts";
+import { gen } from "$fresh/src/dev/gen.ts";
 import manifest from "./fresh.gen.ts";
 
 await gen(manifest);
